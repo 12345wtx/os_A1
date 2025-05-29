2da8ce5e1ded4d602cd7beecaac1b2c1c2aee8ea
Tongxin Wu <160842831+12345wtx@users.noreply.github.com>
2025-05-29 23:02:13 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[]p = new int[n];
+        int[]copy = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = in.nextInt();
+            copy[i] = p[i];
+        }
+        Arrays.sort(copy);
+        int maxVal = 0;
+        for (int i = 0; i < k; i++) {
+            maxVal += (copy[n-1-i] % 998244353);
+        }
+        System.out.print((maxVal % 998244353) + " ");
+        int []pos = new int[k];
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
